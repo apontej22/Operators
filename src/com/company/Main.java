a59953e56119aba9d4a956836242808f58056c27
apontej22 <japonte@students.mchenry.edu>
2017-09-10 12:49:26 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code
+        int a = 23;
+        int b = 5;
+        float result1 = a/b ;
+        float remainder = a % b;
+
+        System.out.println("Assignment Operators" );
+        System.out.println("a = 23" );
+        System.out.println("b = 5" );
+        System.out.println("\nAddition: a + b = " + (a + b) );
+        System.out.println("Subtraction: a - b = " + (a - b) );
+        System.out.println("Multiplication: a * b = " + (a * b) );
+        System.out.println("Division: a / b = " + result1 );
+        System.out.println("Remainder: a % b = " + remainder);
+
+
+    }
+}
